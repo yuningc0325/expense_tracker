5d87031f7b27704b40557ae7aaef97302e0ff1cf
Rick Chang <yuningc0325@gmail.com>
2018-11-10 13:50:59 +0800

    test router set

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>{{msg}}</h1>
+</template>
+
+<script>
+export default{
+    name:'test',
+    data(){
+        return{
+            msg:'Hello world'
+        }
+    }
+}
+</script>
